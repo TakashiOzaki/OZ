935a61572c16f4adb82d5743e69728de79a25e15
ozakitak <ozakitak@nds-g.co.jp>
2014-07-08 19:53:43 +0900

    Userモデルのテストコードおよび機能、実装済

--- src/app/Model/User.php
+++ src/app/Model/User.php
@@ -1,14 +1,36 @@
 <?php
 App::uses('AppModel', 'Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
 /**
  * User Model
  *
  */
 class User extends AppModel {
-    
     public $validate = array(
         'username' => array(
-            'rule' => 'email'
+            'email' => array(
+                'rule' => 'email',
+                'message' => 'ログインIDには、E-mailを入力してください',
+            ),
+            'isUnique' => array(
+                'rule' => 'isUnique',
+                'message' => '既に登録されたログインIDです',
+            ),
+        ),
+        'password' => array(
+            'rule' => array('minLength', 8),
+            'message' => 'パスワードには、8文字以上入力してください',
         )
     );
+    
+    public function beforeSave($options = array()) {
+        
+        IF(!$this->id){
+            //パスワードハッシュ化
+            $passwordHasher = new SimplePasswordHasher();
+            $this->data['User']['password'] = $passwordHasher->hash($this->data['User']['password']);            
+        }
+        return TRUE;
+    }
+
 }
