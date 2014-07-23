d110279b82488abfb92ef420117cbbc06a1dc762
ozakitak <ozakitak@nds-g.co.jp>
2014-07-23 14:16:10 +0900

    アカウント登録コントローラ、テストコードの一部実装

--- src/app/Test/Case/Controller/UsersControllerTest.php
+++ src/app/Test/Case/Controller/UsersControllerTest.php
@@ -15,50 +15,92 @@ class UsersControllerTest extends ControllerTestCase {
 	public $fixtures = array(
 		'app.user'
 	);
+        
+    public function CheckKeywordOfAccountRegistrationScreen() {
+        return 'アカウント登録画面';
+    }
+    public function CheckKeywordOfLoginedScreen() {
+        return '名刺登録画面';
+    }
 
-/**
- * testIndex method
- *
- * @return void
- */
-	public function testIndex() {
-		$this->markTestIncomplete('testIndex not implemented.');
+    public function TestUserData() {
+        return $data = [
+            'User' => [
+                'username' => 'test1@test.co.jp',
+                'password' => 'testpass',
+            ],
+        ];
+    }
+    public function CheckKeywordOfAccountRegistrationError() {
+        return '登録できませんでした';
+    }
+
+//機能：直接URLを指定することでアカウント登録画面を表示する
+//テスト：/usersを指定して、Actionした場合にアカウント登録画面を表示しているか
+	public function test_Viewing_the_account_registration_screen_By_NoneAction(){
+            //第2引数にarray('return' => 'view')を指定すると、
+            //$resultには、取得したhtmlが格納される(らしいです)。
+            $result = $this->testAction('/users', array('return' => 'view'));
+            debug($result);
+            $this->assertTextContains(CheckKeywordOfAccountRegistrationScreen, $result);
+	}
+//機能：ログイン済みであれば画面は表示されない
+//テスト：
+//ログイン後、/usersを指定して、Actionした場合にアカウント登録画面を表示しないか
+//ログインには、アカウント登録機能がされていることが前提となる
+	public function test_Viewing_the_account_registration_screen_By_NoneAction_after_login(){
+            
+            $result1 = $this->testAction('/users/register', array('return' => 'view'),array('data' => TestUserData));
+            $result2 = $this->testAction('/users/register', array('return' => 'view'));
+            debug($result2);
+            $this->assertTextNotContains(CheckKeywordOfAccountRegistrationScreen, $result2);
 	}
 
-/**
- * testView method
- *
- * @return void
- */
-	public function testView() {
-		$this->markTestIncomplete('testView not implemented.');
-	}
+//機能：問題がなければ、データベースにユーザーを登録する
+//テスト：★検討中
+//
 
-/**
- * testAdd method
- *
- * @return void
- */
-	public function testAdd() {
-		$this->markTestIncomplete('testAdd not implemented.');
+//機能：登録できない場合は理由を示したエラー表示を行う
+//テスト：
+//アカウント登録を失敗させ、エラーメッセージの表示を確認する
+//登録失敗には、モックを使い、saveメソッドでFalseを返却する
+	public function test_Viewing_the_account_registration_Error_Message(){
+            
+            $model = $this->getMockForModel('User'); 
+            $model->expects($this->once())->method('save')->will($this->returnValue(FALSE));
+            $this->testAction('/users/register', array('return' => 'view'),array('data' => TestUserData));
+            $result = $this->testAction('/users/register', array('return' => 'view'));
+            debug($result);
+            $this->assertTextNotContains(CheckKeywordOfAccountRegistrationError, $result);
 	}
+//機能：登録できた場合は、同時にログインも行う
+//テスト：登録してみて、ログイン状態を確認する
+	public function test_Login_by_the_account_registration_success(){
+            
+            $result  = $this->testAction('/users/register', array('return' => 'view'),array('data' => TestUserData));
+            $login  =  $this->controller->Auth->login();
+            debug($result);
+            debug($login);
+            $this->assertTrue($login);
+	}
+//
+//機能：登録できた場合は、次のページに遷移する
+//テスト：
+//アカウント登録し、ログイン済みの画面が表示されるか確認する
+	public function test_Viewing_the_logined_screen(){
+        
+            $this->testAction('/users/register', array('return' => 'view'),array('data' => TestUserData));
+            $result = $this->testAction('/users/register', array('return' => 'view'));
+            debug($result);
+            $this->assertTextContains(CheckKeywordOfLoginedScreen, $result);
+	}
+//機能：直接URLを指定することでログイン画面を表示する
+//機能：ログイン済みであれば画面は表示されない
+//機能：未登録のログインIDを入力した場合、ログインできない
+//機能：誤ったパスワードを入力した場合、ログインできない
+//機能：ログインできない場合は理由を示したエラー表示を行う(ただし、ログインIDとパスワードのどちらが誤っているかは表示しない)
+//機能：ログインできた場合は、次のページに遷移する
 
-/**
- * testEdit method
- *
- * @return void
- */
-	public function testEdit() {
-		$this->markTestIncomplete('testEdit not implemented.');
-	}
 
-/**
- * testDelete method
- *
- * @return void
- */
-	public function testDelete() {
-		$this->markTestIncomplete('testDelete not implemented.');
-	}
 
 }
